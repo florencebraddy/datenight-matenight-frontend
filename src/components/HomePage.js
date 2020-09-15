345de5a3934622c097110387d5d1b67341bf6edc
Florence Braddy <florencebraddy@hotmail.com>
2020-09-14 20:48:05 -0400

    transparent navbar

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,23 +1,25 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-// import Grid from "@material-ui/core/Grid";
+import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Button from "./Button";
 import SectionHeaders from "./SectionHeaders";
 import NavBar from "./NavBar";
+import Paper from "@material-ui/core/paper";
 
 const backgroundImage =
   "https://images.unsplash.com/photo-1512187849-463fdb898f21?ixlib=rb-1.2.1&auto=format&fit=crop&w=1778&q=80";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
-    display: "flex"
+    flexGrow: 1
+    // margin: "100px",
+    // padding: "100px"
   },
   background: {
-    backgroundImage: `url(${backgroundImage})`,
     backgroundPosition: "center",
+    backgroundColor: "transparent",
     position: "absolute",
     left: 0,
     right: 0,
@@ -27,16 +29,21 @@ const useStyles = makeStyles(theme => ({
     backgroundRepeat: "no-repeat"
   },
   button: {
-    display: "flex",
-    justifyContent: "bottom",
     size: "large"
   },
   container: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(14),
+    // marginTop: theme.spacing(500),
+    // marginBottom: theme.spacing(14),
     display: "flex",
     flexDirection: "column",
     alignItems: "center"
+  },
+  navbar: {
+    // color: "transparent"
+  },
+  typography: {
+    color: "black",
+    display: "flex"
   }
 }));
 
@@ -45,16 +52,38 @@ export default function HomePage() {
 
   return (
     <div className={classes.root}>
-      <div className={classes.background}>
-        <Container className={classes.container}>
-          <NavBar></NavBar>
-          <Typography color="white" align="center" variant="h1" marked="center">
+      <Container className={classes.container}>
+        <div>
+          <img classesName={classes.background} src={backgroundImage} alt="" />
+          <NavBar className={classes.navbar}></NavBar>
+          <Typography className={classes.typography} variant="h1">
             DATE NIGHT | MATE NIGHT
           </Typography>
-          <Button classesName={classes.button}>Sign Up Now!</Button>
-          {/* <SectionHeaders></SectionHeaders> */}
-          {/* <Grid container spacing={8}> */}
-          {/* <Grid item xs={12}>
+        </div>
+
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>CHEEKY SIGN UP HERE</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>USER REVIEWS HERE</Paper>
+        </Grid>
+        <Grid item xs={5}>
+          <Paper className={classes.paper}>SEARCH DATE NIGHTS</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>LOGIN</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+      </Container>
+
+      {/* <SectionHeaders></SectionHeaders> */}
+      {/* <Grid container spacing={8}> */}
+      {/* <Grid item xs={12}>
             <Paper className={classes.paper}>DATE NIGHT | MATE NIGHT</Paper>
           </Grid>
           <Grid item xs={6}>
@@ -76,8 +105,9 @@ export default function HomePage() {
             <Paper className={classes.paper}>xs=3</Paper>
           </Grid>
               </Grid> */}
-        </Container>
-      </div>
+      {/* <Container>
+        <SectionHeaders></SectionHeaders>
+      </Container> */}
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,6 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
@@ -10,19 +9,28 @@ import MenuIcon from "@material-ui/icons/Menu";
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    margin: "0px"
+    marginBottom: "20px",
+    marginTop: "20px"
   },
   menuButton: {
     marginRight: theme.spacing(10)
   },
   main: {
-    backgroundColor: "#e4ffff",
+    backgroundColor: "transparent",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    borderBottomWidth: 0,
     color: "black",
     opacity: 0.9
+  },
+  button: {
+    // flex
   }
 }));
 
-export default function ButtonAppBar() {
+export default function NavBar() {
   const classes = useStyles();
 
   return (
